e588701906669848a6a3cea7af563226a32a7bba
yingkun <925712087@qq.com>
2020-06-11 20:22:04 +0800

    登录和登出

--- src/online/luffyk/service/impl/UserLoginServiceImpl.java
+++ src/online/luffyk/service/impl/UserLoginServiceImpl.java
@@ -4,12 +4,21 @@ import online.luffyk.dao.UserLoginDao;
 import online.luffyk.dao.impl.UserLoginDaoImpl;
 import online.luffyk.domain.User;
 import online.luffyk.service.UserLoginService;
+import org.apache.log4j.Logger;
 
 public class UserLoginServiceImpl implements UserLoginService {
+    private Logger logger = Logger.getLogger(UserLoginServiceImpl.class);
     private UserLoginDao userLoginDao = new UserLoginDaoImpl();
 
     @Override
     public User checkUserLoginService(String username, String password) {
-        return userLoginDao.checkUserLoginDao(username, password);
+        logger.debug("用户"+username+"发起了登录请求");
+        User user = userLoginDao.checkUserLoginDao(username, password);
+        if(user!=null){
+            logger.debug("用户"+username+"登录成功");
+        }else{
+            logger.debug("用户"+username+"登录失败");
+        }
+        return user;
     }
 }
--- src/online/luffyk/servlet/UserServlet.java
+++ src/online/luffyk/servlet/UserServlet.java
@@ -3,33 +3,56 @@ package online.luffyk.servlet;
 import online.luffyk.domain.User;
 import online.luffyk.service.UserLoginService;
 import online.luffyk.service.impl.UserLoginServiceImpl;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
     private UserLoginService userLoginService = new UserLoginServiceImpl();
+    private Logger logger = Logger.getLogger(UserServlet.class);
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
         String oper = req.getParameter("oper");
-        System.out.println("oper===="+oper);
+        logger.info("oper===="+oper);
         if(oper.equals("login")){
             checkUserLogin(req,resp);
+        }else if(oper.equals("logout")){
+            logger.info("用户准备退出");
+            checkUserLogOut(req,resp);
+        }else{
+            logger.info("没有找到对应的操作符");
         }
     }
 
-    private void checkUserLogin(HttpServletRequest req, HttpServletResponse resp) {
+    private void checkUserLogOut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //获取session
+        HttpSession session = req.getSession();
+        logger.debug("用户"+((User) session.getAttribute("user")).getUsername()+"退出了");
+        //使session失效
+        session.invalidate();
+        //重定向到login页面
+        resp.sendRedirect("/manager/login.jsp");
+    }
+
+    private void checkUserLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String username = req.getParameter("username");
-        System.out.println("username===="+username);
         String password = req.getParameter("password");
-        System.out.println("password===="+password);
         User user = userLoginService.checkUserLoginService(username, password);
-        System.out.println("user===="+user);
+        logger.debug(user);
+        if(user!=null){
+            HttpSession session = req.getSession();
+            session.setAttribute("user",user);
+            resp.sendRedirect("http://127.0.0.1:8080/manager/main/main.jsp");
+        }else{
+            req.getRequestDispatcher("http://127.0.0.1:8080/manager/login.jsp").forward(req,resp);
+        }
     }
 
 
